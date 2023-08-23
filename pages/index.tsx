527a0479fd9a61fc3fd37a686f8ce9ef77474bf0
Maxi-echaide <maxi.echaide@gmail.com>
2023-08-23 19:49:40 -0300

    billboard component,random movie endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { getSession } from 'next-auth/react';
 import { NextPageContext } from 'next';
-import Navbar from '@/components/navbar';
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+
 
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -24,6 +26,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Billboard /> 
     </>
   );
 } 
